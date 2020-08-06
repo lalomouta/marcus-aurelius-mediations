7ed6b228fe5950863df6cf8740701f33ee80fc32
LaloMouta <lalo.mouta@gmail.com>
2020-08-06 12:41:50 +0200

    leyendo fichero

--- src/main/java/com/netvuvu/file/File.java
+++ src/main/java/com/netvuvu/file/File.java
@@ -8,9 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class File {
+
+    FileReader fichero;
+
+    public File(String filename) {
+        FileReader fichero = new FileReader(filename);
+    }
+
     public String[] readLinesIntoArray(String filename){
-        FileReader fileReader = new FileReader(filename);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        //FileReader fileReader = new FileReader(filename);
+        BufferedReader bufferedReader = new BufferedReader(fichero);
         List<String> lineas = new ArrayList<String>();
         String linea = null;
         while((linea = bufferedReader.readLine())!=null){
--- src/main/java/com/netvuvu/main/MarcoAurelioMeditations.java
+++ src/main/java/com/netvuvu/main/MarcoAurelioMeditations.java
@@ -1,18 +1,22 @@
 package com.netvuvu.main;
 
 import com.netvuvu.meditations.Meditation;
+import com.netvuvu.file.File;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.List;
+
 
 public class MarcoAurelioMeditations {
     public static void main (String[] args) throws TwitterException {
         Meditation todayMeditation = new Meditation();
 
-
+        File fichero_txt = new File("C:\\Users\\novac\\OneDrive\\Documentos\\100DaysOfCode\\marcus-aurelius-meditations\\paragraph-line-by-line.txt");
+        String[] array_txt = fichero_txt.readLinesIntoArray()
         Integer id = 1;
         String med_text = id.toString() + ".- From my grandfather Verus I learned good morals and the government of my temper.\n From the reputation and remembrance of my father, modesty and a manly character.";
         //la longitud maxima de un tweet son 280 caracteres,
